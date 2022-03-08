94b3e96b77a4fa2aee8523bea597dfd38c4f1833
08161035 <08161035@me.mcu.edu.tw>
2022-03-08 12:00:25 +0800

    1

---
+++ week02-1/main.cpp
@@ -0,0 +1,35 @@
+#include <GL/glut.h>
+#include <stdio.h>///printf()印東西用的
+float mouseX=0,mouseY=0;///新加的
+
+void myTeapot(float x,float y)
+{
+    glPushMatrix();///備份矩陣
+    ///移動會累積，因為它會修改矩陣
+       // glTranslatef(0.5,0.5,0);///右上角
+        glTranslatef(x,y,0);///
+        glColor3f(1,1,0);///黃色
+        glutSolidTeapot(0.1);///茶壺
+    glPopMatrix();///還原矩陣(還原舊的位置)
+}
+void display()
+{
+     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+     myTeapot((mouseX-150)/150.0,-(mouseY-150)/150.0);///新加的
+     glutSwapBuffers();
+}
+void mouse(int button,int state,int x,int y)
+{
+    printf("%d %d %d %d\n",button,state,x,y);
+    mouseX=x;mouseY=y;
+}
+int main(int argc,char** argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week02_1");
+
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMainLoop();
+}
